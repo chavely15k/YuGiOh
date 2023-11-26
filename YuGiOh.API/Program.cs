43f2a1fbcaefea3ea6866e55352d54b13350bdf2
P3ND4 <josecarlospendasrodriguez@gmail.com>
2023-11-26 00:33:28 -0500

    CORS

--- YuGiOh.API/Program.cs
+++ YuGiOh.API/Program.cs
@@ -7,10 +7,25 @@ using YuGiOh.Infrastructure.Service;
 using YuGiOh.Infrastructure.Repository;
 using YuGiOh.Infrastructure.Mappings;
 using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 // using YuGiOh.Infrastructure.Seed;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                                .AllowAnyMethod()
+                                .AllowAnyHeader();
+        });
+});
+
+
 
 // Add services to the container.
 
@@ -40,10 +55,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseCors();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+
+
 
 var serviceProvider = app.Services.CreateScope().ServiceProvider;
 InitializeData(app, serviceProvider);
