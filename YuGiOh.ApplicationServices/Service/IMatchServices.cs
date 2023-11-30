f52b4e279a21ca32128bd4fcf2284ed5a58da1a5
P3ND4 <josecarlospendasrodriguez@gmail.com>
2023-11-30 18:36:32 -0500

    matches and tournament services

---
+++ YuGiOh.ApplicationServices/Service/IMatchServices.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YuGiOh.ApplicationCore.DTO;
+
+namespace YuGiOh.ApplicationServices.Service
+{
+    public interface IMatchService
+    {
+        public Task<bool> UpdateMatch(MatchDto newMatch);
+
+        public Task<IEnumerable<MatchDto>> GetMatchesByPhase(int phase);
+        
+        public Task<IEnumerable<MatchDto>> GetMatchesByTournament(int id);
+
+    }
+}
