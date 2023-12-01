be9d93a7661376d47941d03315f1f01883cc65bd
chavely15k <chavely15k@gmail.com>
2023-11-30 23:13:50 -0500

    match Service

--- YuGiOh.Infrastructure/Service/MatchService.cs
+++ YuGiOh.Infrastructure/Service/MatchService.cs
@@ -15,7 +15,7 @@ namespace YuGiOh.Infrastructure.Service {
         }
 
         public async Task<IEnumerable<MatchDto>> GetMatchesByPhase(MatchByPhaseDto matchByPhaseDto) {
-            var matches = _dataRepository.FindAsync(m => (m.Round == matchByPhaseDto.Round && m.TournamentId == matchByPhaseDto.TournamentId))
+            var matches = _dataRepository.FindAsync(m => (m.Round == matchByPhaseDto.Round && m.TournamentId == matchByPhaseDto.TournamentId));
             return _mapper.Map<IEnumerable<MatchDto>>(matches);
         }
         
